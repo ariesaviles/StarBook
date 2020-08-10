173f9f7e36c96044ce14930da760937263e5013d
ariesaviles <ana50630@uga.edu>
2020-08-09 21:23:22 -0400

    functional

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -8,53 +8,23 @@ class HomePage extends StatelessWidget {
     return new Scaffold(
       backgroundColor: Colors.brown[200],
       body: new Column(
-
-//        children: <Widget>[
-//          new PreferredSize(
-//            preferredSize: Size.fromHeight(90.0),
-//            child: AppBar(
-//              title: Text(
-//              'StarBook',
-//              style: TextStyle(
-//                fontSize: 50.0,
-//                fontFamily: 'Cagile',
-//                ),
-//              ),
-//          backgroundColor: Colors.black,
-//            ),
-//          ),
+        children: <Widget>[
+          new PreferredSize(
+            preferredSize: Size.fromHeight(90.0),
+            child: AppBar(
+              title: Text(
+              'StarBook',
+              style: TextStyle(
+                fontSize: 50.0,
+                fontFamily: 'Cagile',
+                ),
+              ),
+          backgroundColor: Colors.black,
+            ),
+          ),
           new HomePageBody(),
         ],
       )
     );
   }
 } // HomePage
-
-class AnimateOnScroll extends StatefulWidget {
-  @override
-  _AnimateOnScrollState createState() => _AnimateOnScrollState();
-}
-
-class _AnimateOnScrollState extends State<AnimateOnScroll> {
-  final controller = ScrollController();
-  double appBarHeight = 200.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: CustomScrollView(
-        controller: controller,
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: appBarHeight,
-            floating: true,
-            flexibleSpace: LayoutBuilder(
-              builder: ()
-            ),
-          )
-        ],
-      )
-    )
-  }
-}
